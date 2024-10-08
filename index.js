2aa2c17faea0aa5fc4b624730c80fe14ea24b51a
Do Quang Giap <167546581+giapdq04@users.noreply.github.com>
2024-10-08 21:26:32 +0700

    tạo màn hình welcome

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import MenuAuthenticationScreen from './src/workspaces/MenuAuthenticationScreen'
 import LoginScreen from './src/workspaces/LoginScreen'
+import WelcomeScreen from './src/workspaces/WelcomeScreen'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => LoginScreen);
+AppRegistry.registerComponent(appName, () => WelcomeScreen);
