d35269a621a63b8725ff8654089337f3ad574f73
VietPh37030 <145828354+VietPh37030@users.noreply.github.com>
2024-10-13 21:47:56 +0700

    Xóa thư viện nativewin

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import CommentScreen from './src/workspaces/Comment/commentScreen'
 const App = () => {
   return (
     <View style={{flex:1}}>
-      <HomeScreen/>
+      <CommentScreen/>
     </View>
   )
 }
