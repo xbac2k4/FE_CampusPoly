a6d67fe49d56ca0240a4ed89f86985c1184e27d3
huy1004 <huyvq104@gmail.com>
2024-10-17 11:22:21 +0700

    up alert

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-/**
- * @format
- */
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux'; // Nhập khẩu Provider từ react-redux
+import { store } from './src/workspaces/alert/store'; // Nhập khẩu Redux store từ file store của bạn
+import AlertScreen from './src/workspaces/alert/alertScreen';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Bọc AlertScreen bằng Provider
+const Alert = () => (
+  <Provider store={store}>
+    <AlertScreen />
+  </Provider>
+);
 
-AppRegistry.registerComponent(appName, () => App);
+// Đăng ký component
+AppRegistry.registerComponent(appName, () => Alert);
