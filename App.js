bf118ec9a76129020eb77eeea260247018b4d399
Do Quang Giap <167546581+giapdq04@users.noreply.github.com>
2024-11-16 00:22:17 +0700

    hoàn thanh setup notification

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-
-import React from 'react'
-import AppNavigator from './src/navigation/AppNavigator'
-import { Provider } from 'react-redux'
-import { store } from './src/workspaces/Notification/store'
-import { UserProvider } from './src/services/provider/UseContext'
-import { SocketProvider } from './src/services/provider/SocketContext'
+import React, { useEffect } from 'react';
+import { ToastAndroid, Platform } from 'react-native';
+import AppNavigator from './src/navigation/AppNavigator';
+import { Provider } from 'react-redux';
+import { store } from './src/workspaces/Notification/store';
+import { UserProvider } from './src/services/provider/UseContext';
+import { SocketProvider } from './src/services/provider/SocketContext';
+import { request, PERMISSIONS, RESULTS } from 'react-native-permissions';
 
 const App = () => {
+
+  // cho phép thông báo
+  useEffect(() => {
+    const requestNotificationPermission = async () => {
+      if (Platform.OS === 'android' && Platform.Version >= 33) {
+        try {
+          const result = await request(PERMISSIONS.ANDROID.POST_NOTIFICATIONS);
+          if (result === RESULTS.GRANTED) {
+            ToastAndroid.show('Đã cho phép thông báo', ToastAndroid.SHORT);
+          } else {
+            ToastAndroid.show('Không cho phép thông báo', ToastAndroid.SHORT);
+          }
+        } catch (err) {
+          console.warn(err);
+        }
+      } else {
+        ToastAndroid.show('Không cần yêu cầu quyền thông báo', ToastAndroid.SHORT);
+      }
+    };
+
+    requestNotificationPermission();
+  }, []);
+
   return (
     <Provider store={store}>
       <UserProvider>
@@ -15,7 +39,7 @@ const App = () => {
         </SocketProvider>
       </UserProvider>
     </Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
